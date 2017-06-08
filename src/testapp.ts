42a92a9676214d416e45ffd3429932d1d8213826
tomaskraus <tomas.krausik@gmail.com>
2017-06-08 17:54:59 +0200

    testapp: add more tasks

--- src/testapp.ts
+++ src/testapp.ts
@@ -4,11 +4,16 @@ import { Engine } from "./engine/Engine";
 import * as moment from "moment";
 import { TaskHelper } from "./task/TaskHelper";
 
-let engineLoopInterval = 2; //in seconds
+let engineLoopInterval = 3; //in seconds
 let totalRunningTime = 60; //in seconds
 
 let stor = new SimpleStorage();
 stor.addTask(TaskHelper.create("deleteTask", {}, 10, 0));
+stor.addTask(TaskHelper.create("deleteTask", {}, 11, 0));
+stor.addTask(TaskHelper.create("deleteTask", {}, 14, 0));
+// stor.addTask(TaskHelper.create("testTask", {}, 25, 0));
+stor.addTask(TaskHelper.create("deleteTask", {}, 25, 0));
+stor.addTask(TaskHelper.create("deleteTask", {}, 53, 0));
 
 let eng = new Engine(stor, engineLoopInterval);
 
