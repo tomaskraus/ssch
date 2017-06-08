85109fc339c93b5cfd4992b63397be8bb15be26b
tomaskraus <tomas.krausik@gmail.com>
2017-06-08 15:44:29 +0200

    aware of execution time deviations

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -3,6 +3,8 @@ import { SimpleStorage } from "../storage/SimpleStorage";
 import { Scheduler } from "./Scheduler";
 import * as moment from "moment";
 
+let MAX_TIME_ERROR = 250; //in millis
+let LOOP_CORRECTION = 0;
 
 export class Engine {
     readonly loopInterval: number; //in seconds
@@ -11,31 +13,46 @@ export class Engine {
 
     private runnerId?: number;
 
-    private currentTimestamp: number;
-
 
     constructor(loopInterval: number) {
         this.loopInterval = loopInterval;
         this.storage = new SimpleStorage();
         this.scheduler = new Scheduler(this.storage, loopInterval);
+        this.runnerId = null;
+
     }
 
     run(initialTimestamp: number) {
-        this.currentTimestamp = initialTimestamp;
+        let syntheticTime = initialTimestamp;
 
-        let realTimeOffset: number = moment().unix();
-        console.log(`realtime: ${realTimeOffset}`);
+        let initialTimeMillis = moment().valueOf(); //in millis
+        let totalLoopTime = 0;
+        let timeDeviation = 0;
 
         this.runnerId = setInterval(() => {
-            console.log("engine...");
-            //this.scheduler.doLoop(timestamp)
-        }, this.loopInterval  * 1000);
+            totalLoopTime += this.loopInterval;
+            let currentMillis = moment().valueOf();
+            timeDeviation = (currentMillis - initialTimeMillis) - (1000 * totalLoopTime);
+            if (Math.abs(timeDeviation) > MAX_TIME_ERROR) {
+                throw new Error(`time deviation too high [${timeDeviation}]`);
+            }
+            console.log(`  dev: ${timeDeviation}`);
+
+            syntheticTime += this.loopInterval;
+            this.loop(syntheticTime)
+        }, this.loopInterval  * 1000 + LOOP_CORRECTION);
     }
 
     stop() {
         if (this.runnerId !== null) {
             clearInterval(this.runnerId);
         }
+        //else throw new Error("null runner!");
     }
 
+
+    loop(syntheticTimestamp) {
+        console.log(`${syntheticTimestamp}: engine...`);
+        //this.scheduler.doLoop(syntheticTimestamp);
+    }
 }
