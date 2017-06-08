c803ffb8fe897844cc87df02cab16bedd7211f4c
tomaskraus <tomas.krausik@gmail.com>
2017-06-08 14:31:11 +0200

    stop the testapp after 5s

--- src/testapp.ts
+++ src/testapp.ts
@@ -1,9 +1,16 @@
 
 import { Engine } from "./engine/Engine";
 
+let totalRunningTime = 5; //in seconds
 
-let eng = new Engine(3);
+
+let eng = new Engine(2);
 
 eng.run(0);
+
+setTimeout(() => {
+    console.log("stopping engine");
+    eng.stop();
+}, totalRunningTime * 1000);
 
 
