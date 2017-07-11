db60c82da0cd2307716afae27de5cad2270b7304
tomaskraus <tomas.krausik@gmail.com>
2017-07-11 16:47:57 +0200

    prepare to be an npm module
    minimum files to be included, auto-generated dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Engine } from './engine/Engine';
-export { SimpleStorage } from './storage/SimpleStorage';
-export { TaskHelper } from "./task/TaskHelper";
--- dist/storage/StorageInterface.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=StorageInterface.js.map
