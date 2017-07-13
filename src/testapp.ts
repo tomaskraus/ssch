cb950ff5dcb2cd4deec19ac02b3f4bfaa8a19be7
tomaskraus <tomas.krausik@gmail.com>
2017-07-13 20:26:48 +0200

    fix: testapp will not stop. Better error handling

--- src/testapp.ts
+++ src/testapp.ts
@@ -1,5 +1,6 @@
 
 // import {SimpleStorage} from "./storage/SimpleStorage";
+import { StorageInterface } from "./storage/StorageInterface";
 import { MongoStorage } from "./storage/MongoStorage";
 import { Engine } from "./engine/Engine";
 import * as moment from "moment";
@@ -10,28 +11,48 @@ const debug = Debug('ssch:testapp');
 
 debug("start testapp");
 
+let eng: Engine;
+let storage: StorageInterface;
+
 let engineLoopInterval = 3; //in seconds
 let totalRunningTime = 20; //in seconds
 
 // let stor = new SimpleStorage('storageName');
 MongoStorage.getNewInstance('mongodb://localhost:27017/ssch-testapp')
-    .then(storage => { startApp(storage); })
-    .catch(err => { debug("Application error. Reason: [%o]", err); })
+    .then(stor => {
+        storage = stor;
+        eng = new Engine(storage, engineLoopInterval);
+        startApp();
+    })
+    .catch(err => {
+        debug("Application error. Stack trace: [%s]", err.stack);
+        stopApp();
+    })
 
-function startApp(stor) {
-    stor.addTask(TaskHelper.create("deleteTask", { a: "1st" }, 1, 0));
-    stor.addTask(TaskHelper.create("deleteTask", {}, 10, 0));
-    stor.addTask(TaskHelper.create("deleteTask", { a: 1 }, 11, 0));
-    stor.addTask(TaskHelper.create("deleteTask", {}, 14, 0));
-    stor.addTask(TaskHelper.create("testTask", {}, 20, 0));
-    stor.addTask(TaskHelper.create("deleteTask", {}, 20, 0));
-    stor.addTask(TaskHelper.create("longTask", {}, 21, 0));
-    stor.addTask(TaskHelper.create("longTask", { t: 23 }, 23, 0));
-    stor.addTask(TaskHelper.create("longTask", {}, 28, 0));
-    stor.addTask(TaskHelper.create("deleteTask", { file: "abc.txt" }, 25, 0));
-    stor.addTask(TaskHelper.create("deleteTask", {}, 53, 0));
+function stopApp() {
+    debug("call stopApp");
+    if (eng) {
+        eng.stop();
+    }
+    if (storage) {
+        storage.close();
+    }
+}
 
-    let eng = new Engine(stor, engineLoopInterval);
+
+function startApp() {
+    storage.addTask(TaskHelper.create("deleteTask", { a: "1st" }, 1, 0));
+    storage.addTask(TaskHelper.create("deleteTask", {}, 10, 0));
+    storage.addTask(TaskHelper.create("deleteTask", { a: 1 }, 11, 0));
+    storage.addTask(TaskHelper.create("deleteTask", {}, 14, 0));
+    storage.addTask(TaskHelper.create("testTask", {}, 20, 0));
+    storage.addTask(TaskHelper.create("deleteTask", {}, 20, 0));
+    storage.addTask(TaskHelper.create("longTask", {}, 21, 0));
+    storage.addTask(TaskHelper.create("longTask", { t: 23 }, 23, 0));
+    storage.addTask(TaskHelper.create("longTask", {}, 28, 0));
+    storage.addTask(TaskHelper.create("deleteTask", { file: "abc.txt" }, 25, 0));
+    storage.addTask(TaskHelper.create("deleteTask", {}, 53, 0));
+
 
 
     let startTime = moment().unix();
@@ -39,7 +60,7 @@ function startApp(stor) {
 
     setTimeout(() => {
         debug("stopping engine");
-        eng.stop();
+        stopApp();
         let totalTime = moment().unix() - startTime;
         debug("it tooks [%d] second(s)", totalTime);
     }, totalRunningTime * 1000);
